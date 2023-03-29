bbfd4afa4c981730c7b02331fb474714938c8be1
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2023-03-29 23:09:23 +0530

    send email on order placed functionality

--- server/controller/order.controller.js
+++ server/controller/order.controller.js
@@ -1,4 +1,5 @@
 import Order from "../model/order.model.js";
+import nodemailer from "nodemailer";
 
 // order controller
 // POST /api/order
@@ -22,6 +23,39 @@ export const addOrder = async (req, res) => {
 			isPaid,
 			total,
 		});
+
+		// send email
+		const transporter = nodemailer.createTransport({
+			host: "smtp.ethereal.email",
+			port: 587,
+			auth: {
+				user: "santa.metz72@ethereal.email",
+				pass: "9Mw5JT5ZPb7dDgSSbu",
+			},
+		});
+
+		const mailOptions = {
+			from: "santa.metz72@ethereal.email",
+			to: req.user.email,
+			subject: "Order Confirmation",
+			html: `<h1>Thank you for your order</h1>
+      <p>Order ID: ${order._id}</p>
+      <p>Order Items: ${orderItems.map((item) => item.name)}</p>
+      <p>Total: ${total}</p>
+      <p>Order Date: ${order.createdAt}</p>
+      <p>Order Status: ${order.isPaid ? "Paid" : "Not Paid"}</p>
+      <p>Thank you for shopping with us</p>
+      `,
+		};
+
+		transporter.sendMail(mailOptions, (error, info) => {
+			if (error) {
+				console.log(error);
+			} else {
+				console.log("Email sent: " + info.response);
+			}
+		});
+
 		res.status(201).json({
 			success: true,
 			order,
